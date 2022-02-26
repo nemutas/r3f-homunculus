eda9b4e819024e00e45148dc866cd5706ec1a24f
nemutas <inohiro.focru@gmail.com>
2022-02-26 18:15:43 +0900

    add link button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React, { VFC } from 'react';
+import { LinkIconButton } from './LinkIconButton';
 import { TCanvas } from './TCanvas';
 
 export const App: VFC = () => {
 	return (
 		<div style={{ width: '100vw', height: '100vh' }}>
 			<TCanvas />
+			<LinkIconButton imagePath="/assets/icons/github.svg" linkPath="https://github.com/nemutas/r3f-homunculus" />
 		</div>
 	)
 }
